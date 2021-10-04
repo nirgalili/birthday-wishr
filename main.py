e3b180622cfe09c0ca71dcaf01eb76b5a4a0281e
Nir Galili <nirgalili1@gmail.com>
2021-10-04 10:58:52 +0300

    correct bug of inedex row_index

--- main.py
+++ main.py
@@ -26,39 +26,41 @@ bool_df_time_only = bool_df.drop(columns=["name", "email", "year"])
 
 new_df = df_mail_name_year.join(bool_df_time_only)
 
-row_index = 0
+row_index = None
 for (index, row) in new_df.iterrows():
-    if row.month == True and row.day == True:
+    if row.month and row.day:
         row_index = index
 
+if row_index is None:
+    pass
+else:
+    birthday_kid_email = df.iloc[row_index]["email"]
+    birthday_kid_name = df.iloc[row_index]["name"]
+    print(birthday_kid_email)
+    print(birthday_kid_name)
 
-birthday_kid_email = df.iloc[row_index]["email"]
-birthday_kid_name = df.iloc[row_index]["name"]
-print(birthday_kid_email)
-print(birthday_kid_name)
+    # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+    letters_list = [f"letter_{x+1}.txt" for x in range(3)]
+    print(letters_list)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-letters_list = [f"letter_{x+1}.txt" for x in range(3)]
-print(letters_list)
+    picked_letter_file = rnd.choice(letters_list)
+    print(picked_letter_file)
 
-picked_letter_file = rnd.choice(letters_list)
-print(picked_letter_file)
+    with open(f"letter_templates/{picked_letter_file}", "r") as file:
+        filedata = file.read()
 
-with open(f"letter_templates/{picked_letter_file}", "r") as file:
-    filedata = file.read()
+    filedata =filedata.replace("[NAME]", birthday_kid_name)
 
-filedata =filedata.replace("[NAME]", birthday_kid_name)
+    with open("letter_to_send.txt", "w") as file:
+        file.write(filedata)
 
-with open("letter_to_send.txt", "w") as file:
-    file.write(filedata)
+    # 4. Send the letter generated in step 3 to that person's email address.
 
-# 4. Send the letter generated in step 3 to that person's email address.
-
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="nirgalili1@gmail.com",
-                        msg=f"Subject:Happy Birthday {birthday_kid_name}\n\n{filedata}")
+    # with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    #     connection.starttls()
+    #     connection.login(user=my_email, password=password)
+    #     connection.sendmail(from_addr=my_email,
+    #                         to_addrs="nirgalili1@gmail.com",
+    #                         msg=f"Subject:Happy Birthday {birthday_kid_name}\n\n{filedata}")
 
 
