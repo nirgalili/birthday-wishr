11d34040881fbe13a1c3404d5262e69678a04b84
Nir Galili <nirgalili1@gmail.com>
2021-10-04 13:13:19 +0300

    corrected

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ month = now.month
 day = now.day
 
 df = pd.read_csv("birthdays.csv")
-print(df)
+# print(df)
 df_mail_name_year = df.drop(columns=["month", "day"])
 
 bool_df = df.isin([month, day])
-print(bool_df)
+# print(bool_df)
 
 bool_df_time_only = bool_df.drop(columns=["name", "email", "year"])
 
@@ -36,15 +36,15 @@ if row_index is None:
 else:
     birthday_kid_email = df.iloc[row_index]["email"]
     birthday_kid_name = df.iloc[row_index]["name"]
-    print(birthday_kid_email)
-    print(birthday_kid_name)
+    # print(birthday_kid_email)
+    # print(birthday_kid_name)
 
     # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
     letters_list = [f"letter_{x+1}.txt" for x in range(3)]
-    print(letters_list)
+    # print(letters_list)
 
     picked_letter_file = rnd.choice(letters_list)
-    print(picked_letter_file)
+    # print(picked_letter_file)
 
     with open(f"letter_templates/{picked_letter_file}", "r") as file:
         filedata = file.read()
@@ -56,11 +56,11 @@ else:
 
     # 4. Send the letter generated in step 3 to that person's email address.
 
-    # with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    #     connection.starttls()
-    #     connection.login(user=my_email, password=password)
-    #     connection.sendmail(from_addr=my_email,
-    #                         to_addrs="nirgalili1@gmail.com",
-    #                         msg=f"Subject:Happy Birthday {birthday_kid_name}\n\n{filedata}")
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="nirgalili1@gmail.com",
+                            msg=f"Subject:Happy Birthday {birthday_kid_name}\n\n{filedata}")
 
 
